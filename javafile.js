e4896be6dee10271ab4a1106134c2601c8f9425e
e1git <124605239+e1git@users.noreply.github.com>
2026-04-15 22:10:37 +0000

    e

--- javafile.js
+++ javafile.js
@@ -11,6 +11,53 @@ function drawTriangle(ctx, x, y, size, color) {
   ctx.fill();
 }
 
+function drawHorse(ctx, x, y) {
+  ctx.strokeStyle = "black";
+  ctx.lineWidth = 2;
+
+  // Body
+  ctx.beginPath();
+  ctx.ellipse(x + 50, y + 50, 40, 20, 0, 0, Math.PI * 2);
+  ctx.stroke();
+
+  // Head
+  ctx.beginPath();
+  ctx.ellipse(x + 100, y + 30, 15, 20, 0, 0, Math.PI * 2);
+  ctx.stroke();
+
+  // Neck
+  ctx.beginPath();
+  ctx.moveTo(x + 80, y + 40);
+  ctx.lineTo(x + 90, y + 30);
+  ctx.stroke();
+
+  // Legs
+  ctx.beginPath();
+  ctx.moveTo(x + 30, y + 70);
+  ctx.lineTo(x + 30, y + 100);
+  ctx.moveTo(x + 50, y + 70);
+  ctx.lineTo(x + 50, y + 100);
+  ctx.moveTo(x + 60, y + 70);
+  ctx.lineTo(x + 60, y + 100);
+  ctx.moveTo(x + 80, y + 70);
+  ctx.lineTo(x + 80, y + 100);
+  ctx.stroke();
+
+  // Tail
+  ctx.beginPath();
+  ctx.moveTo(x + 10, y + 50);
+  ctx.lineTo(x, y + 40);
+  ctx.stroke();
+
+  // Mane
+  ctx.beginPath();
+  ctx.moveTo(x + 90, y + 20);
+  ctx.lineTo(x + 95, y + 10);
+  ctx.moveTo(x + 95, y + 20);
+  ctx.lineTo(x + 100, y + 10);
+  ctx.stroke();
+}
+
 function draw(ctx) {
   ctx.fillStyle = "rgb(200 0 0)";
   ctx.fillRect(10, 10, 50, 50);
@@ -26,3 +73,4 @@ const ctx = canvas.getContext("2d");
 //draw(ctx);
 drawTriangle(ctx, 100, 1, 40, "rgb(0 200 0)");
 drawTriangle(ctx, 150, 1, 40, "rgb(200, 0, 150)");
+drawHorse(ctx, 50, 50);
