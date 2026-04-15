ef2941557101ec4006778effd3c2305556549c9a
e1git <124605239+e1git@users.noreply.github.com>
2026-04-15 21:23:12 +0000

    2

--- javafile.js
+++ javafile.js
@@ -3,7 +3,10 @@ function drawTriangle(ctx, x, y, size, color) {
   ctx.beginPath();
   ctx.moveTo(x, y);                      // top vertex
   ctx.lineTo(x + size, y + size);        // bottom right
-  ctx.lineTo(x - size, y + size);        // bottom left
+  ctx.lineTo(x + size, y + size + 100);        // bottom right
+  ctx.lineTo(x - size, y + size + 100);        // bottom left
+  ctx.lineTo(x - size, y + size );
+
   ctx.closePath();
   ctx.fill();
 }
@@ -16,9 +19,9 @@ function draw(ctx) {
   ctx.fillRect(30, 30, 150, 150);
   //ok
 }
-  const canvas = document.getElementById("Canvas");
-  const ctx = canvas.getContext("2d");
+
+const canvas = document.getElementById("Canvas");
+const ctx = canvas.getContext("2d");
+
 draw(ctx);
 drawTriangle(ctx, 100, 1, 40, "rgb(0 200 0)");
-// the hello world program
-console.log('Hello World');
