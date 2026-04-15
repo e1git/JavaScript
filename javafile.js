bea3079412297aa5b61a00a49a006815d7578d37
e1git <124605239+e1git@users.noreply.github.com>
2026-04-15 21:05:34 +0000

    w

--- javafile.js
+++ javafile.js
@@ -22,7 +22,7 @@ function draw(ctx) {
 }
   const canvas = document.getElementById("Canvas");
   const ctx = canvas.getContext("2d");
-///draw(ctx);
-drawTriangle(ctx, 100, 50, 40, "rgb(0 200 0)");
+draw(ctx);
+drawTriangle(ctx, 100, 1, 40, "rgb(0 200 0)");
 // the hello world program
 console.log('Hello World');
